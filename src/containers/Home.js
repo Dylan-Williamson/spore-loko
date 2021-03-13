98209a3180ea263a46c9509af8dd61abd886e2e6
github username <dirtyassweed@icloud.com>
2021-03-13 14:14:17 -0500

    updates products

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -14,49 +14,49 @@ function Home() {
                 <div className="home__row">
                     <Product
                         id="1"
-                        title='The Lean Startup'
-                        price={29.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+                        title='Rasta Imposta Ultimate Banana Fruit Costume Adult Unisex One Size Yellow'
+                        price={24.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/71fILawxg5L._AC_UY741_.jpg"
                         rating={4}
                     />
                     <Product
                         id="2"
-                        title='The Lean Startup'
-                        price={29.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-                        rating={4}
+                        title='Yesito Chicken Harness Hen Size with 6ft Matching Leash'
+                        price={15.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/71Q26wBEIeL._AC_SX425_.jpg"
+                        rating={2}
                     />
                 </div>
                 <div className="home__row">
                     <Product
                         id="3"
-                        title='The Lean Startup'
-                        price={29.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-                        rating={3}
+                        title='Thanos Swimsuit Male One Piece Swimwear for Men'
+                        price={2449.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61jFyhk5WfL._AC_UX679_.jpg"
+                        rating={4}
                     />
                     <Product
                         id="4"
-                        title='The Lean Startup'
-                        price={29.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-                        rating={4}
+                        title='Rubber Chicken Purse - The Hen Bag Handbag'
+                        price={37.95}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61eiIozbjeL._AC_SL1200_.jpg"
+                        rating={3}
                     />
                     <Product
                         id="5"
-                        title='The Lean Startup'
-                        price={29.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-                        rating={4}
+                        title='Nicolas Cage Pillow Case 18" x 18" Inches Covers for Bedroom Safa Gifts, Double-Sided Printed'
+                        price={9.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81K%2Bo6n85WL._AC_SL1500_.jpg"
+                        rating={5}
                     />
                 </div>
                 <div className="home__row">
                     <Product
                         key={(1 + Math.random() * (100-1))}
                         id="6"
-                        title='The Lean Startup'
-                        price={29.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+                        title='Elon Musk Air Freshener'
+                        price={5.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51ateCgRV1L._AC_SL1000_.jpg"
                         rating={4}
                     />
                 </div>
